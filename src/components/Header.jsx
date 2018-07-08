42664cd61e4b07c1df7fee6889ef8c20d301a1ad
beaufoyt <beaufoyyy@live.com>
2018-07-08 19:01:16 +0100

    Improves header background transition

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,57 +2,58 @@ import React from 'react';
 
 import BrandLogo from './BrandLogo';
 import NavLink from './NavLink';
-import PureComponent from './PureComponent';
 
-class Header extends PureComponent {
-    componentDidMount() {
-        this.toggleHeaderBackground();
-        window.addEventListener('scroll', this.handleScroll);
-    }
+const Header = () => {
+    // componentDidMount() {
+    //     this.toggleHeaderBackground();
+    //     window.addEventListener('scroll', this.handleScroll);
+    // }
+    //
+    // componentWillUnmount() {
+    //     window.removeEventListener('scroll', this.handleScroll);
+    // }
 
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.handleScroll);
-    }
+    // getDocumentScrollOffset = () => {
+    //     const doc = document.documentElement;
+    //
+    //     return {
+    //         yOffset: (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0),
+    //         xOffset: (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0),
+    //     };
+    // }
 
-    getDocumentScrollOffset = () => {
-        const doc = document.documentElement;
+    // toggleHeaderBackground = () => {
+    //     const header = document.getElementById('header');
+    //     const { yOffset } = this.getDocumentScrollOffset();
+    //
+    //     if (document.location.pathname === '/') {
+    //         if (yOffset >= 744 && !header.classList.contains('header-dark')) {
+    //             header.classList.toggle('header-dark');
+    //         } else if (yOffset < 744 && header.classList.contains('header-dark')) {
+    //             header.classList.toggle('header-dark');
+    //         }
+    //     }
+    // }
+    //
+    // handleScroll = () => {
+    //     this.toggleHeaderBackground();
+    // };
 
-        return {
-            yOffset: (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0),
-            xOffset: (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0),
-        };
-    }
+    return (
+        <div>
 
-    toggleHeaderBackground = () => {
-        const header = document.getElementById('header');
-        const { yOffset } = this.getDocumentScrollOffset();
-
-        if (document.location.pathname === '/') {
-            if (yOffset >= 744 && !header.classList.contains('header-dark')) {
-                header.classList.toggle('header-dark');
-            } else if (yOffset < 744 && header.classList.contains('header-dark')) {
-                header.classList.toggle('header-dark');
-            }
-        }
-    }
-
-    handleScroll = () => {
-        this.toggleHeaderBackground();
-    };
-
-    render() {
-        return (
-            <div id="header" className="header">
-                <div className="header-content-holder">
-                    <BrandLogo />
+            <div id="header" className="header header-dark" />
+            <div className="header-content-holder">
+                <BrandLogo />
+                <div className="header-nav">
                     <NavLink text="Latest" path="/#latest" />
                     <NavLink text="Recipes" path="/recipes" />
                     <NavLink text="About" path="#" />
                     <NavLink text="Contact" path="#" />
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default Header;
